485adff906438e3f9d811a24328313cf20a7b7ae
Shashank Sapre <shashank.sapre@obconnect.io>
2026-07-06 19:21:51 +0530

    More Pages

--- src/static_copy.py
+++ src/static_copy.py
@@ -10,7 +10,7 @@ def dir_copy(src_path: str, dest_path: str):
     for dir_item in dir_list:
         src_item = os.path.join(src_path, dir_item)
         dest_item = os.path.join(dest_path, dir_item)
-        if os.path.isfile(os.path.join(src_path, dir_item)):
+        if os.path.isfile(src_item):
             shutil.copy(src_item, dest_item)
         else:
             dir_copy(src_item, dest_item)
